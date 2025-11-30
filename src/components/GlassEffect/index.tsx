8c25932cfca870c5ab7f8e3507d4fe9bf2aeb53a
linkava <ovsannikovdana91@gmail.com>
2025-11-30 22:36:09 +0100

    Певний прогрес вже є. Плита прозорості вже відображається. Можу побачити ефект роботи деяких з потрібних характеристик, типу Refraction, Depth, Dispersion, Frost

--- src/components/GlassEffect/index.tsx
+++ src/components/GlassEffect/index.tsx
@@ -1,9 +1,16 @@
-import { Environment, MeshTransmissionMaterial, Text } from "@react-three/drei";
+import {
+    Html,
+    MeshTransmissionMaterial,
+    RoundedBox,
+    Text,
+    Environment,
+} from "@react-three/drei";
 import { Canvas, useThree, type Camera } from "@react-three/fiber";
 import * as THREE from "three";
 import regularFont from "./Sahitya-Regular.ttf";
 import "./styles.scss";
-import { useControls } from "leva";
+import svgLogo from "./logo.svg";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export default function GlassEffect() {
     return (
@@ -13,63 +20,90 @@ export default function GlassEffect() {
             camera={{ position: [0, 0, 10] }}
             style={{ height: "100%", width: "100%" }}
         >
-            <Environment preset="city" />
-            {/*<ambientLight intensity={Math.PI} />*/}
-            <BluredPlate />
-            {/*<pointLight position={[10, 10, 10]} intensity={2} />*/}
-            {/*<mesh position={[0, 0, 6]}>
-                <boxGeometry args={[4, 2, 0]} />
-                <MeshTransmissionMaterial
-                    transmissionSampler
-                    transmission={1}
-                    thickness={0.5}
-                    roughness={0.05}
-                    ior={1.45}
-                    chromaticAberration={0.03}
-                    attenuationDistance={1}
-                    anisotropicBlur={2}
-                    attenuationColor="#ffffff"
-                />
-            </mesh>*/}
-            <BackgroundText />
-        </Canvas>
-    );
-}
+            <color attach="background" args={["#ffffff"]} />
 
-function BluredPlate() {
-    return (
-        <mesh position={[0, 0, 6]}>
-            <boxGeometry args={[4, 2, 1]} />
-            <MeshTransmissionMaterial
-            
+            {/* Додай Environment для коректного освітлення */}
+            <Environment preset="studio" environmentIntensity={1.5} />
+
+            <ambientLight intensity={1.5} color={0xffffff} />
+            <directionalLight
+                position={[10, 10, 10]}
+                intensity={1}
+                color={0xffffff}
             />
-        </mesh>
+
+            <BackgroundText />
+            <Logo />
+        </Canvas>
     );
 }
 
 function BackgroundText() {
     const s = useThree();
-
     const fontSizeInPx = 30;
 
     return (
         <Text
-            position={[0, 0, 5]}
+            position={[0, 0, 0]}
             font={regularFont}
             color="black"
             maxWidth={pixelsToWorld(
                 s.camera,
                 s.size.width - 50,
-                5,
+                10,
                 s.size.height,
             )}
-            fontSize={pixelsToWorld(s.camera, fontSizeInPx, 5, s.size.height)}
+            fontSize={pixelsToWorld(s.camera, fontSizeInPx, 10, s.size.height)}
         >
             TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
             TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
             TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
             TEST TEST TEST TEST TEST TEST TEST
         </Text>
+    );
+}
+
+interface RectInfo {
+    startX: number;
+    startY: number;
+    width: number;
+    height: number;
+}
+
+function Logo() {
+    const htmlSectionRef = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
+        if (!htmlSectionRef.current) return;
+    });
+
+    return (
+        <>
+            <mesh position={[0, 0, 2]}>
+                <RoundedBox args={[4, 2, 0.5]} radius={0.08} smoothness={4}>
+                    <MeshTransmissionMaterial
+                        transmission={0.85}
+                        thickness={0.2}
+                        ior={1.2}
+                        samples={64}
+                        roughness={0.04}
+                        anisotropicBlur={0.2}
+                        attenuationColor="#ffffff"
+                        attenuationDistance={2}
+                        depthWrite={true}
+                        depthTest={true}
+                        toneMapped={false}
+                        chromaticAberration={0.5}
+                    />
+                </RoundedBox>
+            </mesh>
+            <Html center>
+                <section className="glassLogo" ref={htmlSectionRef}>
+                    <img src={svgLogo} alt="logo" />
+                    <span>SoundInfluencers</span>
+                </section>
+            </Html>
+        </>
     );
 }
 
@@ -80,7 +114,6 @@ function pixelsToWorld(
     canvasHeight: number,
 ): number {
     if (camera instanceof THREE.PerspectiveCamera) {
-        // worldHeight на відстані distance від камери
         const fovInRad = (camera.fov * Math.PI) / 180;
         const worldHeight = 2 * Math.tan(fovInRad / 2) * distance;
         return (pixels / canvasHeight) * worldHeight;
